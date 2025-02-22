606f1baa1ffa8cab107533ad025b730c7138753f
Arthur <arthurnaocopia@gmail.com>
2025-02-22 12:20:24 -0300

    fix: fixed font imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
-import type { Metadata } from "next";
-import {Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins
+import type { Metadata } from "next";
+import {Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata: Metadata = {
   title: "FSW Donalds",
