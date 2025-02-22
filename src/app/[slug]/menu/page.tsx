726489055a3aec80b31b12e79e335fd59bc6bcff
Arthur <arthurnaocopia@gmail.com>
2025-02-22 12:23:05 -0300

    feat: add initial style of menu page

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,43 @@
+import { ConsumptionMethod } from "@prisma/client";
+import { notFound } from "next/navigation";
+
+import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+
+import RestaurantHeader from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return Object.values(ConsumptionMethod).includes(
+    consumptionMethod.toUpperCase() as ConsumptionMethod,
+  );
+};
+
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+
+  if (!isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+
+  const restaurant = await getRestaurantBySlug(slug);
+
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant} />
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
