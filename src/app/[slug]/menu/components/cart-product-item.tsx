857d3e034f2200521dc4c7c4e5a4a28586036ac6
Arthur <arthurnaocopia@gmail.com>
2025-03-04 15:18:31 -0300

    feat: add cart item component

---
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -0,0 +1,64 @@
+import { MinusIcon, PlusIcon, TrashIcon } from "lucide-react";
+import Image from "next/image";
+import { useContext } from "react";
+
+import { Button } from "@/components/ui/button";
+import { formatCurrency } from "@/helpers/format-currency";
+
+import { CartContext, CartProduct } from "../contexts/cart";
+
+interface CartItemProps {
+  product: CartProduct;
+}
+
+const CartProductItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity, increaseProductQuantity, removeProduct } =
+    useContext(CartContext);
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3">
+        <div className="relative h-20 w-20 rounded-xl bg-gray-100">
+          <Image src={product.imageUrl} alt={product.name} fill />
+        </div>
+        <div className="space-y-1">
+          <p className="max-w-[90%] truncate text-ellipsis text-xs">
+            {product.name}
+          </p>
+          <p className="text-sm font-semibold">
+            {formatCurrency(product.price)}
+          </p>
+          <div className="flex items-center gap-5">
+            <div className="flex items-center gap-1 text-center">
+              <Button
+                className="h-7 w-7 rounded-lg"
+                variant={product.quantity > 1 ? "outline" : "ghost"}
+                onClick={() => decreaseProductQuantity(product.id)}
+                disabled={product.quantity <= 1}
+              >
+                <MinusIcon />
+              </Button>
+              <p className="w-7 text-xs">{product.quantity}</p>
+              <Button
+                className="h-7 w-7 rounded-lg"
+                variant="destructive"
+                onClick={() => increaseProductQuantity(product.id)}
+              >
+                <PlusIcon />
+              </Button>
+            </div>
+            <Button
+              className="h-7 w-7 rounded-lg"
+              variant="outline"
+              onClick={() => removeProduct(product.id)}
+            >
+              <TrashIcon />
+            </Button>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
+
+export default CartProductItem;
